8a5fc2e800ab59b177a4547b5617e38a334a1c93
Dmitriy Sharshakov aka. sh7dm <sh7dm@outlook.com>
2017-12-25 22:55:47 +0300

    Added moving and keyboard handling

--- src/Game/index.js
+++ src/Game/index.js
@@ -1,5 +1,5 @@
-// import * as PIXI from 'pixi.js'
 import SpriteSheetAnimation from './AnimationLoader'
+import keyboard from './keyboard'
 import AstronautAnimationJSON from '../assets/textrures/Astronaut/AstronautSpritesheet.json'
 
 class Game {
@@ -16,25 +16,69 @@ class Game {
     // move the sprite to the center of the screen
     this.a.sprite.x = this.a.pixi.renderer.width / 2
     this.a.sprite.y = this.a.pixi.renderer.height / 2
-    this.a.sprite.play('walk')
 
     this.a.pixi.stage.addChild(this.a.sprite)
 
-    /* this.a.keys = []
-    for (let i = 0; i < 223; i++) {
-      this.a.keys[i] = false
+    this.a.vx = 0
+    this.a.vy = 0
+
+    let left = keyboard(37)
+    let up = keyboard(38)
+    let right = keyboard(39)
+    let down = keyboard(40)
+
+    left.press = () => {
+      this.a.vx = -5
+      this.a.vy = 0
     }
-    this.a.$el.addEventListener('keydown', event => {
-      this.a.keys[event.keyCode] = true
-    })
-    this.a.$el.addEventListener('keyup', event => {
-      this.a.keys[event.keyCode] = false
-    }) */
+
+    left.release = () => {
+      if (!right.isDown && this.a.vy === 0) {
+        this.a.vx = 0
+      }
+    }
+
+    up.press = () => {
+      this.a.vy = -5
+      this.a.vx = 0
+    }
+    up.release = () => {
+      if (!down.isDown && this.a.vx === 0) {
+        this.a.vy = 0
+      }
+    }
+
+    right.press = () => {
+      this.a.vx = 5
+      this.a.vy = 0
+    }
+    right.release = () => {
+      if (!left.isDown && this.a.vy === 0) {
+        this.a.vx = 0
+      }
+    }
+
+    down.press = () => {
+      this.a.vy = 5
+      this.a.vx = 0
+    }
+    down.release = () => {
+      if (!up.isDown && this.a.vx === 0) {
+        this.a.vy = 0
+      }
+    }
   }
 
   tick (delta) {
-    // this.a.sprite.rotation += 0.1 * delta
+    if (this.a.vx >= 1 || this.a.vy >= 1 || this.a.vx <= -1 || this.a.vy <= -1) {
+      this.a.sprite.play('walk')
+    } else {
+      this.a.sprite.stop()
+    }
+
+    this.a.sprite.x += this.a.vx * delta
+    this.a.sprite.y += this.a.vy * delta
   }
 }
 
-export default { Game: Game, SpriteSheetAnimation: SpriteSheetAnimation }
+export default { Game: Game }
